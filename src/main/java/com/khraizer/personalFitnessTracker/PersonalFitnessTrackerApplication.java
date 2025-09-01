532f8d2b2e8f8ef6c032ecade3cfb156dcb8727b
Khraizer <miguelararat999@gmail.com>
2025-09-01 12:31:07 -0500

    tada: initialize project with Maven structure, basic source and test files

---
+++ src/main/java/com/khraizer/personalFitnessTracker/PersonalFitnessTrackerApplication.java
@@ -0,0 +1,20 @@
+package com.khraizer.personalFitnessTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class to bootstrap the Personal Fitness Tracker Spring Boot application.
+ */
+@SpringBootApplication
+public class PersonalFitnessTrackerApplication {
+
+	/**
+	 * Application entry point.
+	 * Starts the Spring Boot application.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFitnessTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khraizer/personalFitnessTracker/PersonalFitnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khraizer.personalFitnessTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFitnessTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
